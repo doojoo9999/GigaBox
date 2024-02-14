96b0e6a6c0782186226f42303dd99a0663f2b7f5
doojoo9999 <parkbr95@naver.com>
2024-02-14 15:36:22 +0900

    first commit

---
+++ src/main/kotlin/com/teamsparta/gigabox/GigaBoxApplication.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.gigabox
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GigaBoxApplication
+
+fun main(args: Array<String>) {
+    runApplication<GigaBoxApplication>(*args)
+}
