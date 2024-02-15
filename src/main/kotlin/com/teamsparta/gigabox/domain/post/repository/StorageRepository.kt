a02ceca90ff1082e065dd1cc11a69e5284c16eb1
rim109 <fla7722@naver.com>
2024-02-16 00:48:21 +0900

    style : 포맷팅

--- src/main/kotlin/com/teamsparta/gigabox/domain/post/repository/StorageRepository.kt
+++ src/main/kotlin/com/teamsparta/gigabox/domain/post/repository/StorageRepository.kt
@@ -3,5 +3,5 @@ package com.teamsparta.gigabox.domain.post.repository
 import com.teamsparta.gigabox.domain.post.model.Storage
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface StorageRepository: JpaRepository<Storage,Long> {
+interface StorageRepository : JpaRepository<Storage, Long> {
 }
