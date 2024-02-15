1c29ef6e071d1528e3fd234e931bae55598b4fae
doojoo9999 <parkbr95@naver.com>
2024-02-15 16:09:33 +0900

    feat : Mail 구현

--- src/main/kotlin/com/teamsparta/gigabox/domain/member/controller/MemberController.kt
+++ src/main/kotlin/com/teamsparta/gigabox/domain/member/controller/MemberController.kt
@@ -1,6 +1,7 @@
 package com.teamsparta.gigabox.domain.member.controller
 
 import com.teamsparta.gigabox.domain.member.dto.request.SignUpRequest
+import com.teamsparta.gigabox.domain.member.service.MemberService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -8,12 +9,17 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 
 @RequestMapping("/api/v1/member")
-class MemberController {
+class MemberController(
+    private val memberService: MemberService
+) {
 
     @PostMapping("/signup")
     fun singUp(
         @RequestBody request : SignUpRequest
     ) : ResponseEntity<Unit>{
+
+        memberService.signUp(request)
+
         return ResponseEntity
             .status(HttpStatus.OK)
             .build()
--- src/main/kotlin/com/teamsparta/gigabox/domain/member/service/MemberServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/gigabox/domain/member/service/MemberServiceImpl.kt
@@ -4,15 +4,19 @@ import com.teamsparta.gigabox.domain.member.dto.request.SignUpRequest
 import com.teamsparta.gigabox.domain.member.model.MemberEntity
 import com.teamsparta.gigabox.domain.member.model.UserRole
 import com.teamsparta.gigabox.domain.member.repository.MemberRepository
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
 
+@Service
 class MemberServiceImpl(
-    private val memberRepository : MemberRepository
+    private val memberRepository : MemberRepository,
+    private val passwordEncoder : PasswordEncoder,
 ) : MemberService {
     override fun signUp(request: SignUpRequest) {
 
         memberRepository.save(MemberEntity(
             account = request.account,
-            password = request.password,
+            password = passwordEncoder.encode(request.password),
             birthDate = request.birthDate,
             phoneNumber = request.phoneNumber,
             role = UserRole.PRE_MEMBER
