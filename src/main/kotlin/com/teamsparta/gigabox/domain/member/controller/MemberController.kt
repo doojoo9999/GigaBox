efee59fd9b9a4c406b53a6c4a0b1410dc5cd763b
doojoo9999 <parkbr95@naver.com>
2024-02-15 13:22:08 +0900

    feat : MemberController signup 생성

---
+++ src/main/kotlin/com/teamsparta/gigabox/domain/member/controller/MemberController.kt
@@ -0,0 +1,14 @@
+package com.teamsparta.gigabox.domain.member.controller
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@RequestMapping("/api/v1/member")
+class MemberController {
+
+    @PostMapping("/signup")
+    fun singUp(){
+
+    }
+
+}
