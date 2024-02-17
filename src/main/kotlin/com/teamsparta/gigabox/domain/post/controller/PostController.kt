639c5e2ff6dced311e1b3393eadbd41e0b7993d7
rim109 <fla7722@naver.com>
2024-02-18 02:52:33 +0900

    feat : @Operation 추가
    - 어떤 기능인지 간단하게 요약 및 설명함.

--- src/main/kotlin/com/teamsparta/gigabox/domain/post/controller/PostController.kt
+++ src/main/kotlin/com/teamsparta/gigabox/domain/post/controller/PostController.kt
@@ -3,6 +3,7 @@ package com.teamsparta.gigabox.domain.post.controller
 import com.teamsparta.gigabox.domain.post.dto.request.PostRequest
 import com.teamsparta.gigabox.domain.post.dto.response.PostResponse
 import com.teamsparta.gigabox.domain.post.service.PostService
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.*
 class PostController(
     private val postService: PostService
 ) {
+    @Operation(summary = "게시글 조회 + Paging", description = "게시글 전체 조회를 페이징으로 볼 수 있다.")
     @GetMapping("/posts")
     fun getListPost(
         @PageableDefault(size = 5, sort = ["createdAt"], direction = Sort.Direction.DESC) pageable: Pageable
@@ -24,6 +26,7 @@ class PostController(
         return ResponseEntity.status(HttpStatus.OK).body(postService.getListPost(pageable))
     }
 
+    @Operation(summary = "게시글 단건 조회", description = "게시글 단건 조회를 할 수 있다.")
     @GetMapping("/posts/{postId}")
     fun getPost(
         @PathVariable postId: Long
@@ -31,6 +34,7 @@ class PostController(
         return ResponseEntity.status(HttpStatus.OK).body(postService.getPost(postId))
     }
 
+    @Operation(summary = "게시글 등록", description = "게시글을 등록할 수 있다.")
     @PostMapping("/posts", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun createPost(
         @ModelAttribute formData: PostRequest
@@ -38,6 +42,7 @@ class PostController(
         return ResponseEntity.status(HttpStatus.CREATED).body(postService.createPost(formData))
     }
 
+    @Operation(summary = "게시글 수정", description = "게시글을 수정할 수 있다.")
     @PatchMapping("/posts/{postId}/storages/{storageId}", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun updatePost(
         @PathVariable postId: Long,
@@ -47,6 +52,7 @@ class PostController(
         return ResponseEntity.status(HttpStatus.OK).body(postService.updatePost(postId, storageId, formData))
     }
 
+    @Operation(summary = "게시글 삭제", description = "게시글을 삭제할 수 있다.")
     @DeleteMapping("/posts/{postId}")
     fun deletePost(
         @PathVariable postId: Long,
--- src/main/kotlin/com/teamsparta/gigabox/domain/upload/controller/UploadController.kt
+++ src/main/kotlin/com/teamsparta/gigabox/domain/upload/controller/UploadController.kt
@@ -2,6 +2,7 @@ package com.teamsparta.gigabox.domain.upload.controller
 
 import com.teamsparta.gigabox.domain.upload.dto.request.UploadRequest
 import com.teamsparta.gigabox.domain.upload.service.UploadService
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
@@ -14,6 +15,7 @@ class UploadController(
     private val uploadService: UploadService
 ) {
 
+    @Operation(summary = "파일 업로드", description = "파일을 업로드 할 수 있다.")
     @PostMapping("/upload", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun fileUpload(
         @ModelAttribute multiFile: UploadRequest
@@ -21,6 +23,7 @@ class UploadController(
         return ResponseEntity.status(HttpStatus.OK).body(uploadService.fileUpload(multiFile))
     }
 
+    @Operation(summary = "파일 삭제", description = "파일을 삭제 할 수 있다.")
     @DeleteMapping("/upload/{uploadId}")
     fun fileDelete(
         @PathVariable uploadId: Long
