29038eed9d3ef5876680827aa8d2c257c6cd3b3e
doojoo9999 <parkbr95@naver.com>
2024-02-21 02:11:37 +0900

    test : 테스트 성공

--- src/test/kotlin/com/teamsparta/gigabox/domain/coupon/service/CouponServiceTest.kt
+++ src/test/kotlin/com/teamsparta/gigabox/domain/coupon/service/CouponServiceTest.kt
@@ -16,66 +16,71 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executors
+import java.util.stream.Collectors
 
 
 @SpringBootTest
 @ExtendWith(SpringExtension::class)
 class CouponServiceTest {
 
-//    @Autowired
-//    private lateinit var couponService: CouponService
-//    @Test
-//    fun testGetCommonCoupon() {
-//        val threadPool = Executors.newFixedThreadPool(100)
-//
-//        val futures = (1..100).map {
-//            CompletableFuture.supplyAsync({
-//                // getcouponrequest 불러오기
-//                val request = GetCouponRequest(couponNumber = "testcoupon")
-//                //서비스 돌리기
-//                couponService.getCommonCoupon(request)
-//            }, threadPool)
-//        }
-//
-//        //요청이 끝날 때 까지 기다려
-//        CompletableFuture.allOf(*futures.toTypedArray()).join()
-//
-//        val results = futures.stream().map { it.join() }.collect(Collectors.toList())
-//
-//    }
-
-
     @Autowired
-    private lateinit var couponService : CouponService
-
-    @MockBean
-    private lateinit var memberRepository : MemberRepository
-
-    @MockBean
-    private lateinit var commonCouponRepository: CommonCouponRepository
+    private lateinit var couponService: CouponService
 
     @Test
-    fun `getCommonCoupon 이 정상적으로 작동했을 때`() {
-        //given
-        val request = GetCouponRequest(couponNumber = "testcoupon")
-        val member = mockk<MemberEntity>()
-        val coupon = mockk<CommonCouponEntity> {
-            every {available} returns (true)
-            every {couponCount} returns 300
-            every {useCount} returns 0
+    fun testGetCommonCoupon() {
+        val threadPool = Executors.newFixedThreadPool(100)
+
+        val futures = (1..100).map {
+            CompletableFuture.supplyAsync({
+                // getcouponrequest 불러오기
+                val request = GetCouponRequest(couponNumber = "testcoupon")
+                //서비스 돌리기
+                couponService.getCommonCoupon(request)
+            }, threadPool)
         }
 
-        every { memberRepository.findByIdOrNull(eq(2L))} returns member
-        every { commonCouponRepository.findByCouponNumber(coupon.couponNumber) } returns coupon
+        //요청이 끝날 때 까지 기다려
+        CompletableFuture.allOf(*futures.toTypedArray()).join()
 
-        // when
-        val response = couponService.getCommonCoupon(request)
-
-        // then
-        assertEquals(request.couponNumber, response.couponNumber)
-        verify { commonCouponRepository.incUseCount(request.couponNumber) }
-        verify { commonCouponRepository.save(coupon) }
+        val results = futures.stream().map { it.join() }.collect(Collectors.toList())
 
     }
+}
+
+
+//    @Autowired
+//    private lateinit var couponService : CouponService
+//
+//    @MockBean
+//    private lateinit var memberRepository : MemberRepository
+//
+//    @MockBean
+//    private lateinit var commonCouponRepository: CommonCouponRepository
+//
+//    @Test
+//    fun `getCommonCoupon 이 정상적으로 작동했을 때`() {
+//        //given
+//        val request = GetCouponRequest(couponNumber = "testcoupon")
+//        val member = mockk<MemberEntity>()
+//        val coupon = mockk<CommonCouponEntity> {
+//            every {available} returns (true)
+//            every {couponCount} returns 300
+//            every {useCount} returns 0
+//        }
+//
+//        every { memberRepository.findByIdOrNull(eq(2L))} returns member
+//        every { commonCouponRepository.findByCouponNumber(coupon.couponNumber) } returns coupon
+//
+//        // when
+//        val response = couponService.getCommonCoupon(request)
+//
+//        // then
+//        assertEquals(request.couponNumber, response.couponNumber)
+//        verify { commonCouponRepository.incUseCount(request.couponNumber) }
+//        verify { commonCouponRepository.save(coupon) }
+//
+//    }
 
 }
